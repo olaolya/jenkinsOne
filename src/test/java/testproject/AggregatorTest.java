278afd407487e37b7f0f7dddb1e237020a86abd0
olaolya <meshc.olya@gmail.com>
2025-12-15 22:17:09 +0300

    Добавлены новые тесты

--- src/test/java/testproject/AggregatorTest.java
+++ src/test/java/testproject/AggregatorTest.java
@@ -27,4 +27,26 @@ public class AggregatorTest {
         agr.addValue(val);
         assertThat(agr.getSum()).isEqualTo(val * 2);
     }
+
+    @Test
+    public void testReset() {
+        Aggregator agr = new Aggregator();
+        agr.addValue(5.0);
+        agr.addValue(3.0);
+        assertThat(agr.getSum()).isEqualTo(8.0);
+
+        agr.reset();
+        assertThat(agr.getSum()).isZero();
+    }
+
+    @Test
+    public void testResetIdempotent() {
+        Aggregator agr = new Aggregator();
+        agr.addValue(6.0);
+        agr.reset();
+        agr.reset();
+        assertThat(agr.getSum()).isZero();
+    }
 }
+
+
